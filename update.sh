c0c4de7c95de59bd662511b925149b5cfd361797
KangproVpn-droid <Kangahmad80202@gmail.com>
2025-06-04 12:12:59 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/KangproVpn-droid/vip/main/limit/menu.zip
+    wget https://raw.githubusercontent.com/KangproVpn-droid/vip/main/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
